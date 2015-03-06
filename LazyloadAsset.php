b5d0328a98813fe235546c655972dee1bc136abb
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:12:52 +0800

    update

--- LazyloadAsset.php
+++ LazyloadAsset.php
@@ -6,25 +6,9 @@ use yii\web\AssetBundle;
 
 class LazyloadAsset extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-lazyload-widget/assets';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
-//    public $css = [];
-    public $js = [];
+    public $js = ['jquery.lazyload.js'];
     public $depends = ['yii\web\JqueryAsset'];
-
-    private function getJs() {
-        return [
-            YII_DEBUG ? 'jquery.lazyload.js' : 'jquery.lazyload.min.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
 
 }
